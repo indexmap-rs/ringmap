indexmap-rs/indexmap

Remove the internal `trait Entries`
This isn't doing anything for us that can't be done with `pub(crate)`
inherent methods instead.

src/map.rs
@@ -38,7 +38,7 @@ use std::collections::hash_map::RandomState;
 
 use self::core::IndexMapCore;
 use crate::util::{third, try_simplify_range};
-use crate::{Bucket, Entries, Equivalent, GetDisjointMutError, HashValue, TryReserveError};
+use crate::{Bucket, Equivalent, GetDisjointMutError, HashValue, TryReserveError};
 
 /// A hash table where the iteration order of the key-value pairs is independent
 /// of the hash values of the keys.
@@ -113,32 +113,6 @@ where
     }
 }
 
-impl<K, V, S> Entries for IndexMap<K, V, S> {
-    type Entry = Bucket<K, V>;
-
-    #[inline]
-    fn into_entries(self) -> Vec<Self::Entry> {
-        self.core.into_entries()
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &[Self::Entry] {
-        self.core.as_entries()
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut [Self::Entry] {
-        self.core.as_entries_mut()
-    }
-
-    fn with_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        self.core.with_entries(f);
-    }
-}
-
 impl<K, V, S> fmt::Debug for IndexMap<K, V, S>
 where
     K: fmt::Debug,
@@ -203,6 +177,28 @@ impl<K, V, S> IndexMap<K, V, S> {
             core: IndexMapCore::new(),
             hash_builder,
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> Vec<Bucket<K, V>> {
+        self.core.into_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &[Bucket<K, V>] {
+        self.core.as_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries_mut(&mut self) -> &mut [Bucket<K, V>] {
+        self.core.as_entries_mut()
+    }
+
+    pub(crate) fn with_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<K, V>]),
+    {
+        self.core.with_entries(f);
     }
 
     /// Return the number of elements the map can hold without reallocating.

src/map/core.rs
@@ -11,15 +11,13 @@ mod entry;
 
 pub mod raw_entry_v1;
 
-use hashbrown::hash_table;
-
-use crate::vec::{self, Vec};
-use crate::TryReserveError;
+use alloc::vec::{self, Vec};
 use core::mem;
 use core::ops::RangeBounds;
+use hashbrown::hash_table;
 
 use crate::util::simplify_range;
-use crate::{Bucket, Equivalent, HashValue};
+use crate::{Bucket, Equivalent, HashValue, TryReserveError};
 
 type Indices = hash_table::HashTable<usize>;
 type Entries<K, V> = Vec<Bucket<K, V>>;
@@ -109,33 +107,6 @@ where
     }
 }
 
-impl<K, V> crate::Entries for IndexMapCore<K, V> {
-    type Entry = Bucket<K, V>;
-
-    #[inline]
-    fn into_entries(self) -> Vec<Self::Entry> {
-        self.entries
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &[Self::Entry] {
-        &self.entries
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut [Self::Entry] {
-        &mut self.entries
-    }
-
-    fn with_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        f(&mut self.entries);
-        self.rebuild_hash_table();
-    }
-}
-
 impl<K, V> IndexMapCore<K, V> {
     /// The maximum capacity before the `entries` allocation would exceed `isize::MAX`.
     const MAX_ENTRIES_CAPACITY: usize = (isize::MAX as usize) / mem::size_of::<Bucket<K, V>>();
@@ -159,6 +130,29 @@ impl<K, V> IndexMapCore<K, V> {
             indices: Indices::with_capacity(n),
             entries: Vec::with_capacity(n),
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> Entries<K, V> {
+        self.entries
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &[Bucket<K, V>] {
+        &self.entries
+    }
+
+    #[inline]
+    pub(crate) fn as_entries_mut(&mut self) -> &mut [Bucket<K, V>] {
+        &mut self.entries
+    }
+
+    pub(crate) fn with_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<K, V>]),
+    {
+        f(&mut self.entries);
+        self.rebuild_hash_table();
     }
 
     #[inline]

src/map/mutable.rs
@@ -1,8 +1,7 @@
 use core::hash::{BuildHasher, Hash};
 
 use super::{
-    Bucket, Entries, Entry, Equivalent, IndexMap, IndexedEntry, IterMut2, OccupiedEntry,
-    VacantEntry,
+    Bucket, Entry, Equivalent, IndexMap, IndexedEntry, IterMut2, OccupiedEntry, VacantEntry,
 };
 
 /// Opt-in mutable access to [`IndexMap`] keys.

src/set.rs
@@ -28,7 +28,7 @@ use core::fmt;
 use core::hash::{BuildHasher, Hash};
 use core::ops::{BitAnd, BitOr, BitXor, Index, RangeBounds, Sub};
 
-use super::{Entries, Equivalent, IndexMap};
+use super::{Equivalent, IndexMap};
 
 type Bucket<T> = super::Bucket<T, ()>;
 
@@ -105,32 +105,6 @@ where
     }
 }
 
-impl<T, S> Entries for IndexSet<T, S> {
-    type Entry = Bucket<T>;
-
-    #[inline]
-    fn into_entries(self) -> Vec<Self::Entry> {
-        self.map.into_entries()
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &[Self::Entry] {
-        self.map.as_entries()
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut [Self::Entry] {
-        self.map.as_entries_mut()
-    }
-
-    fn with_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        self.map.with_entries(f);
-    }
-}
-
 impl<T, S> fmt::Debug for IndexSet<T, S>
 where
     T: fmt::Debug,
@@ -187,6 +161,23 @@ impl<T, S> IndexSet<T, S> {
         IndexSet {
             map: IndexMap::with_hasher(hash_builder),
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> Vec<Bucket<T>> {
+        self.map.into_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &[Bucket<T>] {
+        self.map.as_entries()
+    }
+
+    pub(crate) fn with_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<T>]),
+    {
+        self.map.with_entries(f);
     }
 
     /// Return the number of elements the set can hold without reallocating.

indexmap-rs/ringmap

(cherry picked from commit b698d3b0ef190f480aa826cb8f32a9c23eeecce4)

src/map.rs
@@ -41,7 +41,7 @@ use std::collections::hash_map::RandomState;
 
 use self::core::RingMapCore;
 use crate::util::third;
-use crate::{Bucket, Entries, Equivalent, GetDisjointMutError, HashValue, TryReserveError};
+use crate::{Bucket, Equivalent, GetDisjointMutError, HashValue, TryReserveError};
 
 /// A hash table where the iteration order of the key-value pairs is independent
 /// of the hash values of the keys.
@@ -116,32 +116,6 @@ where
     }
 }
 
-impl<K, V, S> Entries for RingMap<K, V, S> {
-    type Entry = Bucket<K, V>;
-
-    #[inline]
-    fn into_entries(self) -> VecDeque<Self::Entry> {
-        self.core.into_entries()
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &VecDeque<Self::Entry> {
-        self.core.as_entries()
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut VecDeque<Self::Entry> {
-        self.core.as_entries_mut()
-    }
-
-    fn with_contiguous_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        self.core.with_contiguous_entries(f);
-    }
-}
-
 impl<K, V, S> fmt::Debug for RingMap<K, V, S>
 where
     K: fmt::Debug,
@@ -204,6 +178,28 @@ impl<K, V, S> RingMap<K, V, S> {
             core: RingMapCore::new(),
             hash_builder,
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> VecDeque<Bucket<K, V>> {
+        self.core.into_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &VecDeque<Bucket<K, V>> {
+        self.core.as_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries_mut(&mut self) -> &mut VecDeque<Bucket<K, V>> {
+        self.core.as_entries_mut()
+    }
+
+    pub(crate) fn with_contiguous_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<K, V>]),
+    {
+        self.core.with_contiguous_entries(f);
     }
 
     /// Return the number of elements the map can hold without reallocating.

src/map/core.rs
@@ -13,14 +13,13 @@ pub mod raw_entry_v1;
 
 use hashbrown::hash_table;
 
-use crate::vec_deque::{self, VecDeque};
-use crate::TryReserveError;
+use alloc::collections::vec_deque::{self, VecDeque};
 use core::cmp::Ordering;
 use core::ops::RangeBounds;
 use core::{iter, mem, slice};
 
 use crate::util::simplify_range;
-use crate::{Bucket, Equivalent, HashValue};
+use crate::{Bucket, Equivalent, HashValue, TryReserveError};
 
 type Indices = hash_table::HashTable<OffsetIndex>;
 type Entries<K, V> = VecDeque<Bucket<K, V>>;
@@ -180,33 +179,6 @@ where
     }
 }
 
-impl<K, V> crate::Entries for RingMapCore<K, V> {
-    type Entry = Bucket<K, V>;
-
-    #[inline]
-    fn into_entries(self) -> Entries<K, V> {
-        self.entries
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &Entries<K, V> {
-        &self.entries
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut Entries<K, V> {
-        &mut self.entries
-    }
-
-    fn with_contiguous_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        f(self.entries.make_contiguous());
-        self.rebuild_hash_table();
-    }
-}
-
 impl<K, V> RingMapCore<K, V> {
     /// The maximum capacity before the `entries` allocation would exceed `isize::MAX`.
     const MAX_ENTRIES_CAPACITY: usize = (isize::MAX as usize) / mem::size_of::<Bucket<K, V>>();
@@ -232,6 +204,29 @@ impl<K, V> RingMapCore<K, V> {
             entries: VecDeque::with_capacity(n),
             offset: 0,
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> Entries<K, V> {
+        self.entries
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &Entries<K, V> {
+        &self.entries
+    }
+
+    #[inline]
+    pub(crate) fn as_entries_mut(&mut self) -> &mut Entries<K, V> {
+        &mut self.entries
+    }
+
+    pub(crate) fn with_contiguous_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<K, V>]),
+    {
+        f(self.entries.make_contiguous());
+        self.rebuild_hash_table();
     }
 
     #[inline]

src/map/mutable.rs
@@ -1,7 +1,7 @@
 use core::hash::{BuildHasher, Hash};
 
 use super::{
-    Bucket, Entries, Entry, Equivalent, IndexedEntry, IterMut2, OccupiedEntry, RingMap, VacantEntry,
+    Bucket, Entry, Equivalent, IndexedEntry, IterMut2, OccupiedEntry, RingMap, VacantEntry,
 };
 
 /// Opt-in mutable access to [`RingMap`] keys.

src/set.rs
@@ -28,7 +28,7 @@ use core::fmt;
 use core::hash::{BuildHasher, Hash, Hasher};
 use core::ops::{BitAnd, BitOr, BitXor, Index, RangeBounds, Sub};
 
-use super::{Entries, Equivalent, RingMap};
+use super::{Equivalent, RingMap};
 
 type Bucket<T> = super::Bucket<T, ()>;
 
@@ -105,32 +105,6 @@ where
     }
 }
 
-impl<T, S> Entries for RingSet<T, S> {
-    type Entry = Bucket<T>;
-
-    #[inline]
-    fn into_entries(self) -> VecDeque<Self::Entry> {
-        self.map.into_entries()
-    }
-
-    #[inline]
-    fn as_entries(&self) -> &VecDeque<Self::Entry> {
-        self.map.as_entries()
-    }
-
-    #[inline]
-    fn as_entries_mut(&mut self) -> &mut VecDeque<Self::Entry> {
-        self.map.as_entries_mut()
-    }
-
-    fn with_contiguous_entries<F>(&mut self, f: F)
-    where
-        F: FnOnce(&mut [Self::Entry]),
-    {
-        self.map.with_contiguous_entries(f);
-    }
-}
-
 impl<T, S> fmt::Debug for RingSet<T, S>
 where
     T: fmt::Debug,
@@ -187,6 +161,23 @@ impl<T, S> RingSet<T, S> {
         RingSet {
             map: RingMap::with_hasher(hash_builder),
         }
+    }
+
+    #[inline]
+    pub(crate) fn into_entries(self) -> VecDeque<Bucket<T>> {
+        self.map.into_entries()
+    }
+
+    #[inline]
+    pub(crate) fn as_entries(&self) -> &VecDeque<Bucket<T>> {
+        self.map.as_entries()
+    }
+
+    pub(crate) fn with_contiguous_entries<F>(&mut self, f: F)
+    where
+        F: FnOnce(&mut [Bucket<T>]),
+    {
+        self.map.with_contiguous_entries(f);
     }
 
     /// Return the number of elements the set can hold without reallocating.
@@ -1045,7 +1036,7 @@ impl<T, S> RingSet<T, S> {
     /// Rearranges the internal storage of this map so it is one contiguous slice,
     /// which is then returned.
     pub fn make_contiguous(&mut self) -> &Slice<T> {
-        Slice::from_slice(self.as_entries_mut().make_contiguous())
+        Slice::from_slice(self.map.as_entries_mut().make_contiguous())
     }
 
     /// Converts into a boxed slice of all the values in the set.