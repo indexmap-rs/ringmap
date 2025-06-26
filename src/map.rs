indexmap-rs/indexmap

Add range support to `extract_if`

src/map.rs
@@ -307,7 +307,8 @@ impl<K, V, S> IndexMap<K, V, S> {
         Drain::new(self.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    /// Creates an iterator which uses a closure to determine if an element should be removed,
+    /// for all elements in the given range.
     ///
     /// If the closure returns true, the element is removed from the map and yielded.
     /// If the closure returns false, or panics, the element remains in the map and will not be
@@ -315,6 +316,11 @@ impl<K, V, S> IndexMap<K, V, S> {
     ///
     /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
     /// whether you choose to keep or remove it.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire map, use `RangeFull`
+    /// like `map.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -330,7 +336,7 @@ impl<K, V, S> IndexMap<K, V, S> {
     /// use indexmap::IndexMap;
     ///
     /// let mut map: IndexMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
-    /// let extracted: IndexMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    /// let extracted: IndexMap<i32, i32> = map.extract_if(.., |k, _v| k % 2 == 0).collect();
     ///
     /// let evens = extracted.keys().copied().collect::<Vec<_>>();
     /// let odds = map.keys().copied().collect::<Vec<_>>();
@@ -338,11 +344,12 @@ impl<K, V, S> IndexMap<K, V, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    pub fn extract_if<R, F>(&mut self, range: R, pred: F) -> ExtractIf<'_, K, V, F>
     where
+        R: RangeBounds<usize>,
         F: FnMut(&K, &mut V) -> bool,
     {
-        ExtractIf::new(&mut self.core, pred)
+        ExtractIf::new(&mut self.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/map/iter.rs
@@ -777,21 +777,19 @@ where
 ///
 /// This `struct` is created by [`IndexMap::extract_if()`].
 /// See its documentation for more.
-pub struct ExtractIf<'a, K, V, F>
-where
-    F: FnMut(&K, &mut V) -> bool,
-{
+pub struct ExtractIf<'a, K, V, F> {
     inner: ExtractCore<'a, K, V>,
     pred: F,
 }
 
-impl<K, V, F> ExtractIf<'_, K, V, F>
-where
-    F: FnMut(&K, &mut V) -> bool,
-{
-    pub(super) fn new(core: &mut IndexMapCore<K, V>, pred: F) -> ExtractIf<'_, K, V, F> {
+impl<K, V, F> ExtractIf<'_, K, V, F> {
+    pub(super) fn new<R>(core: &mut IndexMapCore<K, V>, range: R, pred: F) -> ExtractIf<'_, K, V, F>
+    where
+        R: RangeBounds<usize>,
+        F: FnMut(&K, &mut V) -> bool,
+    {
         ExtractIf {
-            inner: core.extract(),
+            inner: core.extract(range),
             pred,
         }
     }
@@ -817,10 +815,7 @@ where
     }
 }
 
-impl<'a, K, V, F> fmt::Debug for ExtractIf<'a, K, V, F>
-where
-    F: FnMut(&K, &mut V) -> bool,
-{
+impl<'a, K, V, F> fmt::Debug for ExtractIf<'a, K, V, F> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ExtractIf").finish_non_exhaustive()
     }

src/set.rs
@@ -257,11 +257,17 @@ impl<T, S> IndexSet<T, S> {
         Drain::new(self.map.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if a value should be removed.
+    /// Creates an iterator which uses a closure to determine if a value should be removed,
+    /// for all values in the given range.
     ///
     /// If the closure returns true, then the value is removed and yielded.
     /// If the closure returns false, the value will remain in the list and will not be yielded
     /// by the iterator.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire set, use `RangeFull`
+    /// like `set.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -277,7 +283,7 @@ impl<T, S> IndexSet<T, S> {
     /// use indexmap::IndexSet;
     ///
     /// let mut set: IndexSet<i32> = (0..8).collect();
-    /// let extracted: IndexSet<i32> = set.extract_if(|v| v % 2 == 0).collect();
+    /// let extracted: IndexSet<i32> = set.extract_if(.., |v| v % 2 == 0).collect();
     ///
     /// let evens = extracted.into_iter().collect::<Vec<_>>();
     /// let odds = set.into_iter().collect::<Vec<_>>();
@@ -285,11 +291,12 @@ impl<T, S> IndexSet<T, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, T, F>
+    pub fn extract_if<R, F>(&mut self, range: R, pred: F) -> ExtractIf<'_, T, F>
     where
+        R: RangeBounds<usize>,
         F: FnMut(&T) -> bool,
     {
-        ExtractIf::new(&mut self.map.core, pred)
+        ExtractIf::new(&mut self.map.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/set/iter.rs
@@ -632,21 +632,19 @@ impl<I: fmt::Debug> fmt::Debug for UnitValue<I> {
 ///
 /// This `struct` is created by [`IndexSet::extract_if()`].
 /// See its documentation for more.
-pub struct ExtractIf<'a, T, F>
-where
-    F: FnMut(&T) -> bool,
-{
+pub struct ExtractIf<'a, T, F> {
     inner: ExtractCore<'a, T, ()>,
     pred: F,
 }
 
-impl<T, F> ExtractIf<'_, T, F>
-where
-    F: FnMut(&T) -> bool,
-{
-    pub(super) fn new(core: &mut IndexMapCore<T, ()>, pred: F) -> ExtractIf<'_, T, F> {
+impl<T, F> ExtractIf<'_, T, F> {
+    pub(super) fn new<R>(core: &mut IndexMapCore<T, ()>, range: R, pred: F) -> ExtractIf<'_, T, F>
+    where
+        R: RangeBounds<usize>,
+        F: FnMut(&T) -> bool,
+    {
         ExtractIf {
-            inner: core.extract(),
+            inner: core.extract(range),
             pred,
         }
     }
@@ -669,10 +667,7 @@ where
     }
 }
 
-impl<'a, T, F> fmt::Debug for ExtractIf<'a, T, F>
-where
-    F: FnMut(&T) -> bool,
-{
+impl<'a, T, F> fmt::Debug for ExtractIf<'a, T, F> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ExtractIf").finish_non_exhaustive()
     }

indexmap-rs/ringmap

(cherry picked from commit a8d7dc50a250971fe2203bdf23c2b5a307db7c86)

src/map.rs
@@ -305,7 +305,8 @@ impl<K, V, S> RingMap<K, V, S> {
         Drain::new(self.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    /// Creates an iterator which uses a closure to determine if an element should be removed,
+    /// for all elements in the given range.
     ///
     /// If the closure returns true, the element is removed from the map and yielded.
     /// If the closure returns false, or panics, the element remains in the map and will not be
@@ -313,6 +314,11 @@ impl<K, V, S> RingMap<K, V, S> {
     ///
     /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
     /// whether you choose to keep or remove it.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire map, use `RangeFull`
+    /// like `map.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -328,7 +334,7 @@ impl<K, V, S> RingMap<K, V, S> {
     /// use ringmap::RingMap;
     ///
     /// let mut map: RingMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
-    /// let extracted: RingMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    /// let extracted: RingMap<i32, i32> = map.extract_if(.., |k, _v| k % 2 == 0).collect();
     ///
     /// let evens = extracted.keys().copied().collect::<Vec<_>>();
     /// let odds = map.keys().copied().collect::<Vec<_>>();
@@ -336,11 +342,12 @@ impl<K, V, S> RingMap<K, V, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    pub fn extract_if<F, R>(&mut self, range: R, pred: F) -> ExtractIf<'_, K, V, F>
     where
         F: FnMut(&K, &mut V) -> bool,
+        R: RangeBounds<usize>,
     {
-        ExtractIf::new(&mut self.core, pred)
+        ExtractIf::new(&mut self.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/map/iter.rs
@@ -1044,21 +1044,19 @@ where
 ///
 /// This `struct` is created by [`RingMap::extract_if()`].
 /// See its documentation for more.
-pub struct ExtractIf<'a, K, V, F>
-where
-    F: FnMut(&K, &mut V) -> bool,
-{
+pub struct ExtractIf<'a, K, V, F> {
     inner: ExtractCore<'a, K, V>,
     pred: F,
 }
 
-impl<K, V, F> ExtractIf<'_, K, V, F>
-where
-    F: FnMut(&K, &mut V) -> bool,
-{
-    pub(super) fn new(core: &mut RingMapCore<K, V>, pred: F) -> ExtractIf<'_, K, V, F> {
+impl<K, V, F> ExtractIf<'_, K, V, F> {
+    pub(super) fn new<R>(core: &mut RingMapCore<K, V>, range: R, pred: F) -> ExtractIf<'_, K, V, F>
+    where
+        R: RangeBounds<usize>,
+        F: FnMut(&K, &mut V) -> bool,
+    {
         ExtractIf {
-            inner: core.extract(),
+            inner: core.extract(range),
             pred,
         }
     }
@@ -1086,7 +1084,8 @@ where
 
 impl<'a, K, V, F> fmt::Debug for ExtractIf<'a, K, V, F>
 where
-    F: FnMut(&K, &mut V) -> bool,
+    K: fmt::Debug,
+    V: fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ExtractIf").finish_non_exhaustive()

src/set.rs
@@ -249,11 +249,17 @@ impl<T, S> RingSet<T, S> {
         Drain::new(self.map.core.drain(range))
     }
 
-    /// Creates an iterator which uses a closure to determine if a value should be removed.
+    /// Creates an iterator which uses a closure to determine if a value should be removed,
+    /// for all values in the given range.
     ///
     /// If the closure returns true, then the value is removed and yielded.
     /// If the closure returns false, the value will remain in the list and will not be yielded
     /// by the iterator.
+    ///
+    /// The range may be any type that implements [`RangeBounds<usize>`],
+    /// including all of the `std::ops::Range*` types, or even a tuple pair of
+    /// `Bound` start and end values. To check the entire set, use `RangeFull`
+    /// like `set.extract_if(.., predicate)`.
     ///
     /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
     /// or the iteration short-circuits, then the remaining elements will be retained.
@@ -269,7 +275,7 @@ impl<T, S> RingSet<T, S> {
     /// use ringmap::RingSet;
     ///
     /// let mut set: RingSet<i32> = (0..8).collect();
-    /// let extracted: RingSet<i32> = set.extract_if(|v| v % 2 == 0).collect();
+    /// let extracted: RingSet<i32> = set.extract_if(.., |v| v % 2 == 0).collect();
     ///
     /// let evens = extracted.into_iter().collect::<Vec<_>>();
     /// let odds = set.into_iter().collect::<Vec<_>>();
@@ -277,11 +283,12 @@ impl<T, S> RingSet<T, S> {
     /// assert_eq!(evens, vec![0, 2, 4, 6]);
     /// assert_eq!(odds, vec![1, 3, 5, 7]);
     /// ```
-    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, T, F>
+    pub fn extract_if<F, R>(&mut self, range: R, pred: F) -> ExtractIf<'_, T, F>
     where
         F: FnMut(&T) -> bool,
+        R: RangeBounds<usize>,
     {
-        ExtractIf::new(&mut self.map.core, pred)
+        ExtractIf::new(&mut self.map.core, range, pred)
     }
 
     /// Splits the collection into two at the given index.

src/set/iter.rs
@@ -630,21 +630,19 @@ impl<I: fmt::Debug> fmt::Debug for UnitValue<I> {
 ///
 /// This `struct` is created by [`RingSet::extract_if()`].
 /// See its documentation for more.
-pub struct ExtractIf<'a, T, F>
-where
-    F: FnMut(&T) -> bool,
-{
+pub struct ExtractIf<'a, T, F> {
     inner: ExtractCore<'a, T, ()>,
     pred: F,
 }
 
-impl<T, F> ExtractIf<'_, T, F>
-where
-    F: FnMut(&T) -> bool,
-{
-    pub(super) fn new(core: &mut RingMapCore<T, ()>, pred: F) -> ExtractIf<'_, T, F> {
+impl<T, F> ExtractIf<'_, T, F> {
+    pub(super) fn new<R>(core: &mut RingMapCore<T, ()>, range: R, pred: F) -> ExtractIf<'_, T, F>
+    where
+        R: RangeBounds<usize>,
+        F: FnMut(&T) -> bool,
+    {
         ExtractIf {
-            inner: core.extract(),
+            inner: core.extract(range),
             pred,
         }
     }
@@ -669,7 +667,7 @@ where
 
 impl<'a, T, F> fmt::Debug for ExtractIf<'a, T, F>
 where
-    F: FnMut(&T) -> bool,
+    T: fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ExtractIf").finish_non_exhaustive()