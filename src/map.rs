indexmap-rs/indexmap

Add map and set `extract_if`

src/map.rs
@@ -15,7 +15,8 @@ mod tests;
 pub use self::core::raw_entry_v1::{self, RawEntryApiV1};
 pub use self::core::{Entry, IndexedEntry, OccupiedEntry, VacantEntry};
 pub use self::iter::{
-    Drain, IntoIter, IntoKeys, IntoValues, Iter, IterMut, Keys, Splice, Values, ValuesMut,
+    Drain, ExtractIf, IntoIter, IntoKeys, IntoValues, Iter, IterMut, Keys, Splice, Values,
+    ValuesMut,
 };
 pub use self::slice::Slice;
 pub use crate::mutable_keys::MutableKeys;
@@ -33,7 +34,7 @@ use alloc::vec::Vec;
 #[cfg(feature = "std")]
 use std::collections::hash_map::RandomState;
 
-use self::core::IndexMapCore;
+pub(crate) use self::core::{ExtractCore, IndexMapCore};
 use crate::util::{third, try_simplify_range};
 use crate::{Bucket, Entries, Equivalent, HashValue, TryReserveError};
 
@@ -299,6 +300,44 @@ impl<K, V, S> IndexMap<K, V, S> {
         R: RangeBounds<usize>,
     {
         Drain::new(self.core.drain(range))
+    }
+
+    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    ///
+    /// If the closure returns true, the element is removed from the map and yielded.
+    /// If the closure returns false, or panics, the element remains in the map and will not be
+    /// yielded.
+    ///
+    /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
+    /// whether you choose to keep or remove it.
+    ///
+    /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
+    /// or the iteration short-circuits, then the remaining elements will be retained.
+    /// Use [`retain`] with a negated predicate if you do not need the returned iterator.
+    ///
+    /// [`retain`]: IndexMap::retain
+    ///
+    /// # Examples
+    ///
+    /// Splitting a map into even and odd keys, reusing the original map:
+    ///
+    /// ```
+    /// use indexmap::IndexMap;
+    ///
+    /// let mut map: IndexMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
+    /// let extracted: IndexMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    ///
+    /// let evens = extracted.keys().copied().collect::<Vec<_>>();
+    /// let odds = map.keys().copied().collect::<Vec<_>>();
+    ///
+    /// assert_eq!(evens, vec![0, 2, 4, 6]);
+    /// assert_eq!(odds, vec![1, 3, 5, 7]);
+    /// ```
+    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    where
+        F: FnMut(&K, &mut V) -> bool,
+    {
+        ExtractIf::new(&mut self.core, pred)
     }
 
     /// Splits the collection into two at the given index.

src/map/iter.rs
@@ -1,5 +1,4 @@
-use super::core::IndexMapCore;
-use super::{Bucket, Entries, IndexMap, Slice};
+use super::{Bucket, Entries, ExtractCore, IndexMap, IndexMapCore, Slice};
 
 use alloc::vec::{self, Vec};
 use core::fmt;
@@ -709,5 +708,58 @@ where
             .field("drain", &self.drain)
             .field("replace_with", &self.replace_with)
             .finish()
+    }
+}
+
+/// An extracting iterator for `IndexMap`.
+///
+/// This `struct` is created by [`IndexMap::extract_if()`].
+/// See its documentation for more.
+pub struct ExtractIf<'a, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    inner: ExtractCore<'a, K, V>,
+    pred: F,
+}
+
+impl<K, V, F> ExtractIf<'_, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    pub(super) fn new(core: &mut IndexMapCore<K, V>, pred: F) -> ExtractIf<'_, K, V, F> {
+        ExtractIf {
+            inner: core.extract(),
+            pred,
+        }
+    }
+}
+
+impl<K, V, F> Iterator for ExtractIf<'_, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner
+            .extract_if(|bucket| {
+                let (key, value) = bucket.ref_mut();
+                (self.pred)(key, value)
+            })
+            .map(Bucket::key_value)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.inner.remaining()))
+    }
+}
+
+impl<'a, K, V, F> fmt::Debug for ExtractIf<'a, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ExtractIf").finish_non_exhaustive()
     }
 }

src/set/iter.rs
@@ -1,3 +1,5 @@
+use crate::map::{ExtractCore, IndexMapCore};
+
 use super::{Bucket, Entries, IndexSet, Slice};
 
 use alloc::vec::{self, Vec};
@@ -622,5 +624,55 @@ where
 impl<I: fmt::Debug> fmt::Debug for UnitValue<I> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&self.0, f)
+    }
+}
+
+/// An extracting iterator for `IndexSet`.
+///
+/// This `struct` is created by [`IndexSet::extract_if()`].
+/// See its documentation for more.
+pub struct ExtractIf<'a, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    inner: ExtractCore<'a, T, ()>,
+    pred: F,
+}
+
+impl<T, F> ExtractIf<'_, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    pub(super) fn new(core: &mut IndexMapCore<T, ()>, pred: F) -> ExtractIf<'_, T, F> {
+        ExtractIf {
+            inner: core.extract(),
+            pred,
+        }
+    }
+}
+
+impl<T, F> Iterator for ExtractIf<'_, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner
+            .extract_if(|bucket| (self.pred)(bucket.key_ref()))
+            .map(Bucket::key)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.inner.remaining()))
+    }
+}
+
+impl<'a, T, F> fmt::Debug for ExtractIf<'a, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ExtractIf").finish_non_exhaustive()
     }
 }

tests/quick.rs
@@ -174,6 +174,47 @@ quickcheck_limit! {
         }
     }
 
+    fn extract_if_odd(insert: Vec<u8>) -> bool {
+        let mut map = IndexMap::new();
+        for &x in &insert {
+            map.insert(x, x.to_string());
+        }
+
+        let (odd, even): (Vec<_>, Vec<_>) = map.keys().copied().partition(|k| k % 2 == 1);
+
+        let extracted: Vec<_> = map
+            .extract_if(|k, _| k % 2 == 1)
+            .map(|(k, _)| k)
+            .collect();
+
+        even.iter().all(|k| map.contains_key(k))
+            && map.keys().eq(&even)
+            && extracted == odd
+    }
+
+    fn extract_if_odd_limit(insert: Vec<u8>, limit: usize) -> bool {
+        let mut map = IndexMap::new();
+        for &x in &insert {
+            map.insert(x, x.to_string());
+        }
+        let limit = limit % (map.len() + 1);
+
+        let mut i = 0;
+        let (odd, other): (Vec<_>, Vec<_>) = map.keys().copied().partition(|k| {
+            k % 2 == 1 && i < limit && { i += 1; true }
+        });
+
+        let extracted: Vec<_> = map
+            .extract_if(|k, _| k % 2 == 1)
+            .map(|(k, _)| k)
+            .take(limit)
+            .collect();
+
+        other.iter().all(|k| map.contains_key(k))
+            && map.keys().eq(&other)
+            && extracted == odd
+    }
+
     fn shift_remove(insert: Vec<u8>, remove: Vec<u8>) -> bool {
         let mut map = IndexMap::new();
         for &key in &insert {

indexmap-rs/ringmap

(cherry picked from commit 5aa1b8319013978b44fe7bd491e521713447d86d)

src/map.rs
@@ -16,7 +16,8 @@ mod tests;
 pub use self::core::raw_entry_v1::{self, RawEntryApiV1};
 pub use self::core::{Entry, IndexedEntry, OccupiedEntry, VacantEntry};
 pub use self::iter::{
-    Drain, IntoIter, IntoKeys, IntoValues, Iter, IterMut, IterMut2, Keys, Splice, Values, ValuesMut,
+    Drain, ExtractIf, IntoIter, IntoKeys, IntoValues, Iter, IterMut, IterMut2, Keys, Splice,
+    Values, ValuesMut,
 };
 pub use self::mutable::MutableEntryKey;
 pub use self::mutable::MutableKeys;
@@ -39,7 +40,7 @@ use alloc::vec::Vec;
 #[cfg(feature = "std")]
 use std::collections::hash_map::RandomState;
 
-use self::core::RingMapCore;
+pub(crate) use self::core::{ExtractCore, RingMapCore};
 use crate::util::third;
 use crate::{Bucket, Equivalent, GetDisjointMutError, HashValue, TryReserveError};
 
@@ -302,6 +303,44 @@ impl<K, V, S> RingMap<K, V, S> {
         R: RangeBounds<usize>,
     {
         Drain::new(self.core.drain(range))
+    }
+
+    /// Creates an iterator which uses a closure to determine if an element should be removed.
+    ///
+    /// If the closure returns true, the element is removed from the map and yielded.
+    /// If the closure returns false, or panics, the element remains in the map and will not be
+    /// yielded.
+    ///
+    /// Note that `extract_if` lets you mutate every value in the filter closure, regardless of
+    /// whether you choose to keep or remove it.
+    ///
+    /// If the returned `ExtractIf` is not exhausted, e.g. because it is dropped without iterating
+    /// or the iteration short-circuits, then the remaining elements will be retained.
+    /// Use [`retain`] with a negated predicate if you do not need the returned iterator.
+    ///
+    /// [`retain`]: RingMap::retain
+    ///
+    /// # Examples
+    ///
+    /// Splitting a map into even and odd keys, reusing the original map:
+    ///
+    /// ```
+    /// use ringmap::RingMap;
+    ///
+    /// let mut map: RingMap<i32, i32> = (0..8).map(|x| (x, x)).collect();
+    /// let extracted: RingMap<i32, i32> = map.extract_if(|k, _v| k % 2 == 0).collect();
+    ///
+    /// let evens = extracted.keys().copied().collect::<Vec<_>>();
+    /// let odds = map.keys().copied().collect::<Vec<_>>();
+    ///
+    /// assert_eq!(evens, vec![0, 2, 4, 6]);
+    /// assert_eq!(odds, vec![1, 3, 5, 7]);
+    /// ```
+    pub fn extract_if<F>(&mut self, pred: F) -> ExtractIf<'_, K, V, F>
+    where
+        F: FnMut(&K, &mut V) -> bool,
+    {
+        ExtractIf::new(&mut self.core, pred)
     }
 
     /// Splits the collection into two at the given index.

src/map/iter.rs
@@ -1,5 +1,4 @@
-use super::core::RingMapCore;
-use super::{Bucket, RingMap};
+use super::{Bucket, ExtractCore, RingMap, RingMapCore};
 
 use alloc::collections::vec_deque::{self, VecDeque};
 use core::hash::{BuildHasher, Hash};
@@ -1038,5 +1037,58 @@ where
             .field("drain", &self.drain)
             .field("replace_with", &self.replace_with)
             .finish()
+    }
+}
+
+/// An extracting iterator for `RingMap`.
+///
+/// This `struct` is created by [`RingMap::extract_if()`].
+/// See its documentation for more.
+pub struct ExtractIf<'a, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    inner: ExtractCore<'a, K, V>,
+    pred: F,
+}
+
+impl<K, V, F> ExtractIf<'_, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    pub(super) fn new(core: &mut RingMapCore<K, V>, pred: F) -> ExtractIf<'_, K, V, F> {
+        ExtractIf {
+            inner: core.extract(),
+            pred,
+        }
+    }
+}
+
+impl<K, V, F> Iterator for ExtractIf<'_, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner
+            .extract_if(|bucket| {
+                let (key, value) = bucket.ref_mut();
+                (self.pred)(key, value)
+            })
+            .map(Bucket::key_value)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.inner.remaining()))
+    }
+}
+
+impl<'a, K, V, F> fmt::Debug for ExtractIf<'a, K, V, F>
+where
+    F: FnMut(&K, &mut V) -> bool,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ExtractIf").finish_non_exhaustive()
     }
 }

src/set/iter.rs
@@ -1,3 +1,5 @@
+use crate::map::{ExtractCore, RingMapCore};
+
 use super::{Bucket, RingSet};
 use crate::map::Buckets;
 
@@ -621,5 +623,55 @@ where
 impl<I: fmt::Debug> fmt::Debug for UnitValue<I> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&self.0, f)
+    }
+}
+
+/// An extracting iterator for `RingSet`.
+///
+/// This `struct` is created by [`RingSet::extract_if()`].
+/// See its documentation for more.
+pub struct ExtractIf<'a, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    inner: ExtractCore<'a, T, ()>,
+    pred: F,
+}
+
+impl<T, F> ExtractIf<'_, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    pub(super) fn new(core: &mut RingMapCore<T, ()>, pred: F) -> ExtractIf<'_, T, F> {
+        ExtractIf {
+            inner: core.extract(),
+            pred,
+        }
+    }
+}
+
+impl<T, F> Iterator for ExtractIf<'_, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner
+            .extract_if(|bucket| (self.pred)(bucket.key_ref()))
+            .map(Bucket::key)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.inner.remaining()))
+    }
+}
+
+impl<'a, T, F> fmt::Debug for ExtractIf<'a, T, F>
+where
+    F: FnMut(&T) -> bool,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("ExtractIf").finish_non_exhaustive()
     }
 }

tests/quick.rs
@@ -231,6 +231,47 @@ quickcheck_limit! {
         }
     }
 
+    fn extract_if_odd(insert: Vec<u8>) -> bool {
+        let mut map = RingMap::new();
+        for &x in &insert {
+            map.insert(x, x.to_string());
+        }
+
+        let (odd, even): (Vec<_>, Vec<_>) = map.keys().copied().partition(|k| k % 2 == 1);
+
+        let extracted: Vec<_> = map
+            .extract_if(|k, _| k % 2 == 1)
+            .map(|(k, _)| k)
+            .collect();
+
+        even.iter().all(|k| map.contains_key(k))
+            && map.keys().eq(&even)
+            && extracted == odd
+    }
+
+    fn extract_if_odd_limit(insert: Vec<u8>, limit: usize) -> bool {
+        let mut map = RingMap::new();
+        for &x in &insert {
+            map.insert(x, x.to_string());
+        }
+        let limit = limit % (map.len() + 1);
+
+        let mut i = 0;
+        let (odd, other): (Vec<_>, Vec<_>) = map.keys().copied().partition(|k| {
+            k % 2 == 1 && i < limit && { i += 1; true }
+        });
+
+        let extracted: Vec<_> = map
+            .extract_if(|k, _| k % 2 == 1)
+            .map(|(k, _)| k)
+            .take(limit)
+            .collect();
+
+        other.iter().all(|k| map.contains_key(k))
+            && map.keys().eq(&other)
+            && extracted == odd
+    }
+
     fn remove(insert: Vec<u8>, remove: Vec<u8>) -> bool {
         let mut map = RingMap::new();
         for &key in &insert {