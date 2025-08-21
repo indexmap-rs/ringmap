25109c2982d9100fb98b8564be9ae0dcb70eea5e
Josh Stone <cuviper@gmail.com>
2025-08-21 14:17:55 -0700

    Take *two* entries in `insert_sorted_by`
    (cherry picked from commit 354345b5a250be9d565e3b60f311a435141e6412)

--- src/map/core/entry.rs
+++ src/map/core/entry.rs
@@ -467,12 +467,14 @@ impl<'a, K, V> VacantEntry<'a, K, V> {
     /// pair is inserted at that position regardless.
     ///
     /// Computes in **O(n)** time (average).
-    pub fn insert_sorted_by<F>(self, value: V, cmp: F) -> (usize, &'a mut V)
+    pub fn insert_sorted_by<F>(self, value: V, mut cmp: F) -> (usize, &'a mut V)
     where
         K: Ord,
-        F: FnMut(&K, &V) -> Ordering,
+        F: FnMut(&K, &V, &K, &V) -> Ordering,
     {
-        let (Ok(i) | Err(i)) = self.map.binary_search_by(cmp);
+        let (Ok(i) | Err(i)) = self
+            .map
+            .binary_search_by(|k, v| cmp(k, v, &self.key, &value));
         (i, self.shift_insert(i, value))
     }
 
--- src/map/tests.rs
+++ src/map/tests.rs
@@ -1346,14 +1346,14 @@ fn insert_sorted_by() {
     let mut values = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)];
     let mut map: RingMap<i32, i32> = RingMap::new();
     for (key, value) in values {
-        let (_, old) = map.insert_sorted_by(key, value, |probe, _| key.cmp(probe));
+        let (_, old) = map.insert_sorted_by(key, value, |key1, _, key2, _| key2.cmp(key1));
         assert_eq!(old, None);
     }
     values.reverse();
     assert_eq!(values, *map.make_contiguous());
 
     for (key, value) in &mut values {
-        let (_, old) = map.insert_sorted_by(*key, -*value, |probe, _| (*key).cmp(probe));
+        let (_, old) = map.insert_sorted_by(*key, -*value, |key1, _, key2, _| key2.cmp(key1));
         assert_eq!(old, Some(*value));
         *value = -*value;
     }
