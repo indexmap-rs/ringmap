indexmap-rs/indexmap

Implement sval2 support
Supports serialization without nesting

src/sval2.rs
@@ -1,0 +1,36 @@
+#![cfg_attr(docsrs, doc(cfg(feature = "serde")))]
+
+use crate::{IndexMap, IndexSet};
+use sval2::{Stream, Value};
+
+impl<K: Value, V: Value, S> Value for IndexMap<K, V, S> {
+    fn stream<'sval, ST: Stream<'sval> + ?Sized>(&'sval self, stream: &mut ST) -> sval2::Result {
+        stream.map_begin(Some(self.len()))?;
+
+        for (k, v) in self {
+            stream.map_key_begin()?;
+            stream.value(k)?;
+            stream.map_key_end()?;
+
+            stream.map_value_begin()?;
+            stream.value(v)?;
+            stream.map_value_end()?;
+        }
+
+        stream.map_end()
+    }
+}
+
+impl<K: Value, S> Value for IndexSet<K, S> {
+    fn stream<'sval, ST: Stream<'sval> + ?Sized>(&'sval self, stream: &mut ST) -> sval2::Result {
+        stream.seq_begin(Some(self.len()))?;
+
+        for value in self {
+            stream.seq_value_begin()?;
+            stream.value(value)?;
+            stream.seq_value_end()?;
+        }
+
+        stream.seq_end()
+    }
+}

test-sval2/src/lib.rs
@@ -1,0 +1,104 @@
+#![cfg(test)]
+
+use fnv::FnvBuildHasher;
+use indexmap::{indexmap, indexset, IndexMap, IndexSet};
+use sval2_test::{assert_tokens, Token};
+
+#[test]
+fn test_sval2_map() {
+    let map = indexmap! { 1 => 2, 3 => 4 };
+    assert_tokens(
+        &map,
+        &[
+            Token::MapBegin(Some(2)),
+            Token::MapKeyBegin,
+            Token::I32(1),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(2),
+            Token::MapValueEnd,
+            Token::MapKeyBegin,
+            Token::I32(3),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(4),
+            Token::MapValueEnd,
+            Token::MapEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_set() {
+    let set = indexset! { 1, 2, 3, 4 };
+    assert_tokens(
+        &set,
+        &[
+            Token::SeqBegin(Some(4)),
+            Token::SeqValueBegin,
+            Token::I32(1),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(2),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(3),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(4),
+            Token::SeqValueEnd,
+            Token::SeqEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_map_fnv_hasher() {
+    let mut map: IndexMap<i32, i32, FnvBuildHasher> = Default::default();
+    map.insert(1, 2);
+    map.insert(3, 4);
+    assert_tokens(
+        &map,
+        &[
+            Token::MapBegin(Some(2)),
+            Token::MapKeyBegin,
+            Token::I32(1),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(2),
+            Token::MapValueEnd,
+            Token::MapKeyBegin,
+            Token::I32(3),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(4),
+            Token::MapValueEnd,
+            Token::MapEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_set_fnv_hasher() {
+    let mut set: IndexSet<i32, FnvBuildHasher> = Default::default();
+    set.extend(1..5);
+    assert_tokens(
+        &set,
+        &[
+            Token::SeqBegin(Some(4)),
+            Token::SeqValueBegin,
+            Token::I32(1),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(2),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(3),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(4),
+            Token::SeqValueEnd,
+            Token::SeqEnd,
+        ],
+    );
+}

indexmap-rs/ringmap

(cherry picked from commit e04f6bf53235406d366adcc2e313c6ed75478756)

src/sval2.rs
@@ -1,0 +1,36 @@
+#![cfg_attr(docsrs, doc(cfg(feature = "serde")))]
+
+use crate::{RingMap, RingSet};
+use sval2::{Stream, Value};
+
+impl<K: Value, V: Value, S> Value for RingMap<K, V, S> {
+    fn stream<'sval, ST: Stream<'sval> + ?Sized>(&'sval self, stream: &mut ST) -> sval2::Result {
+        stream.map_begin(Some(self.len()))?;
+
+        for (k, v) in self {
+            stream.map_key_begin()?;
+            stream.value(k)?;
+            stream.map_key_end()?;
+
+            stream.map_value_begin()?;
+            stream.value(v)?;
+            stream.map_value_end()?;
+        }
+
+        stream.map_end()
+    }
+}
+
+impl<K: Value, S> Value for RingSet<K, S> {
+    fn stream<'sval, ST: Stream<'sval> + ?Sized>(&'sval self, stream: &mut ST) -> sval2::Result {
+        stream.seq_begin(Some(self.len()))?;
+
+        for value in self {
+            stream.seq_value_begin()?;
+            stream.value(value)?;
+            stream.seq_value_end()?;
+        }
+
+        stream.seq_end()
+    }
+}

test-sval2/src/lib.rs
@@ -1,0 +1,104 @@
+#![cfg(test)]
+
+use fnv::FnvBuildHasher;
+use ringmap::{ringmap, ringset, RingMap, RingSet};
+use sval2_test::{assert_tokens, Token};
+
+#[test]
+fn test_sval2_map() {
+    let map = ringmap! { 1 => 2, 3 => 4 };
+    assert_tokens(
+        &map,
+        &[
+            Token::MapBegin(Some(2)),
+            Token::MapKeyBegin,
+            Token::I32(1),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(2),
+            Token::MapValueEnd,
+            Token::MapKeyBegin,
+            Token::I32(3),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(4),
+            Token::MapValueEnd,
+            Token::MapEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_set() {
+    let set = ringset! { 1, 2, 3, 4 };
+    assert_tokens(
+        &set,
+        &[
+            Token::SeqBegin(Some(4)),
+            Token::SeqValueBegin,
+            Token::I32(1),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(2),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(3),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(4),
+            Token::SeqValueEnd,
+            Token::SeqEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_map_fnv_hasher() {
+    let mut map: RingMap<i32, i32, FnvBuildHasher> = Default::default();
+    map.insert(1, 2);
+    map.insert(3, 4);
+    assert_tokens(
+        &map,
+        &[
+            Token::MapBegin(Some(2)),
+            Token::MapKeyBegin,
+            Token::I32(1),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(2),
+            Token::MapValueEnd,
+            Token::MapKeyBegin,
+            Token::I32(3),
+            Token::MapKeyEnd,
+            Token::MapValueBegin,
+            Token::I32(4),
+            Token::MapValueEnd,
+            Token::MapEnd,
+        ],
+    );
+}
+
+#[test]
+fn test_sval2_set_fnv_hasher() {
+    let mut set: RingSet<i32, FnvBuildHasher> = Default::default();
+    set.extend(1..5);
+    assert_tokens(
+        &set,
+        &[
+            Token::SeqBegin(Some(4)),
+            Token::SeqValueBegin,
+            Token::I32(1),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(2),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(3),
+            Token::SeqValueEnd,
+            Token::SeqValueBegin,
+            Token::I32(4),
+            Token::SeqValueEnd,
+            Token::SeqEnd,
+        ],
+    );
+}