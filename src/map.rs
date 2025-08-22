indexmap-rs/indexmap

Add `sort_by_key` and `sort_unstable_by_key`

src/map.rs
@@ -1170,6 +1170,20 @@ impl<K, V, S> IndexMap<K, V, S> {
         IntoIter::new(entries)
     }
 
+    /// Sort the map's key-value pairs in place using a sort-key extraction function.
+    ///
+    /// Computes in **O(n log n + c)** time and **O(n)** space where *n* is
+    /// the length of the map and *c* the capacity. The sort is stable.
+    pub fn sort_by_key<T, F>(&mut self, mut sort_key: F)
+    where
+        T: Ord,
+        F: FnMut(&K, &V) -> T,
+    {
+        self.with_entries(move |entries| {
+            entries.sort_by_key(move |a| sort_key(&a.key, &a.value));
+        });
+    }
+
     /// Sort the map's key-value pairs by the default ordering of the keys, but
     /// may not preserve the order of equal elements.
     ///
@@ -1212,6 +1226,20 @@ impl<K, V, S> IndexMap<K, V, S> {
         let mut entries = self.into_entries();
         entries.sort_unstable_by(move |a, b| cmp(&a.key, &a.value, &b.key, &b.value));
         IntoIter::new(entries)
+    }
+
+    /// Sort the map's key-value pairs in place using a sort-key extraction function.
+    ///
+    /// Computes in **O(n log n + c)** time where *n* is
+    /// the length of the map and *c* is the capacity. The sort is unstable.
+    pub fn sort_unstable_by_key<T, F>(&mut self, mut sort_key: F)
+    where
+        T: Ord,
+        F: FnMut(&K, &V) -> T,
+    {
+        self.with_entries(move |entries| {
+            entries.sort_unstable_by_key(move |a| sort_key(&a.key, &a.value));
+        });
     }
 
     /// Sort the map's key-value pairs in place using a sort-key extraction function.

src/rayon/map.rs
@@ -445,6 +445,18 @@ where
         IntoParIter { entries }
     }
 
+    /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction
+    /// function.
+    pub fn par_sort_by_key<T, F>(&mut self, sort_key: F)
+    where
+        T: Ord,
+        F: Fn(&K, &V) -> T + Sync,
+    {
+        self.with_entries(move |entries| {
+            entries.par_sort_by_key(move |a| sort_key(&a.key, &a.value));
+        });
+    }
+
     /// Sort the map's key-value pairs in parallel, by the default ordering of the keys.
     pub fn par_sort_unstable_keys(&mut self)
     where
@@ -478,6 +490,18 @@ where
         let mut entries = self.into_entries();
         entries.par_sort_unstable_by(move |a, b| cmp(&a.key, &a.value, &b.key, &b.value));
         IntoParIter { entries }
+    }
+
+    /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction
+    /// function.
+    pub fn par_sort_unstable_by_key<T, F>(&mut self, sort_key: F)
+    where
+        T: Ord,
+        F: Fn(&K, &V) -> T + Sync,
+    {
+        self.with_entries(move |entries| {
+            entries.par_sort_unstable_by_key(move |a| sort_key(&a.key, &a.value));
+        });
     }
 
     /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction

src/rayon/set.rs
@@ -516,6 +516,17 @@ where
         IntoParIter { entries }
     }
 
+    /// Sort the set's values in place and in parallel, using a key extraction function.
+    pub fn par_sort_by_key<K, F>(&mut self, sort_key: F)
+    where
+        K: Ord,
+        F: Fn(&T) -> K + Sync,
+    {
+        self.with_entries(move |entries| {
+            entries.par_sort_by_key(move |a| sort_key(&a.key));
+        });
+    }
+
     /// Sort the set's values in parallel by their default ordering.
     pub fn par_sort_unstable(&mut self)
     where
@@ -545,6 +556,17 @@ where
         let mut entries = self.into_entries();
         entries.par_sort_unstable_by(move |a, b| cmp(&a.key, &b.key));
         IntoParIter { entries }
+    }
+
+    /// Sort the set's values in place and in parallel, using a key extraction function.
+    pub fn par_sort_unstable_by_key<K, F>(&mut self, sort_key: F)
+    where
+        K: Ord,
+        F: Fn(&T) -> K + Sync,
+    {
+        self.with_entries(move |entries| {
+            entries.par_sort_unstable_by_key(move |a| sort_key(&a.key));
+        });
     }
 
     /// Sort the set's values in place and in parallel, using a key extraction function.

src/set.rs
@@ -956,6 +956,19 @@ impl<T, S> IndexSet<T, S> {
         IntoIter::new(entries)
     }
 
+    /// Sort the set's values in place using a key extraction function.
+    ///
+    /// Computes in **O(n log n)** time and **O(n)** space. The sort is stable.
+    pub fn sort_by_key<K, F>(&mut self, mut sort_key: F)
+    where
+        K: Ord,
+        F: FnMut(&T) -> K,
+    {
+        self.with_entries(move |entries| {
+            entries.sort_by_key(move |a| sort_key(&a.key));
+        });
+    }
+
     /// Sort the set's values by their default ordering.
     ///
     /// See [`sort_unstable_by`](Self::sort_unstable_by) for details.
@@ -985,6 +998,19 @@ impl<T, S> IndexSet<T, S> {
         let mut entries = self.into_entries();
         entries.sort_unstable_by(move |a, b| cmp(&a.key, &b.key));
         IntoIter::new(entries)
+    }
+
+    /// Sort the set's values in place using a key extraction function.
+    ///
+    /// Computes in **O(n log n)** time. The sort is unstable.
+    pub fn sort_unstable_by_key<K, F>(&mut self, mut sort_key: F)
+    where
+        K: Ord,
+        F: FnMut(&T) -> K,
+    {
+        self.with_entries(move |entries| {
+            entries.sort_unstable_by_key(move |a| sort_key(&a.key));
+        });
     }
 
     /// Sort the set's values in place using a key extraction function.

src/set/tests.rs
@@ -762,6 +762,28 @@ fn first() {
 }
 
 #[test]
+fn sort_by_key() {
+    let mut index_set: IndexSet<i32> = IndexSet::new();
+    index_set.insert(3);
+    index_set.insert(1);
+    index_set.insert(2);
+    index_set.insert(0);
+    index_set.sort_by_key(|&x| -x);
+    assert_eq!(index_set.as_slice(), &[3, 2, 1, 0]);
+}
+
+#[test]
+fn sort_unstable_by_key() {
+    let mut index_set: IndexSet<i32> = IndexSet::new();
+    index_set.insert(3);
+    index_set.insert(1);
+    index_set.insert(2);
+    index_set.insert(0);
+    index_set.sort_unstable_by_key(|&x| -x);
+    assert_eq!(index_set.as_slice(), &[3, 2, 1, 0]);
+}
+
+#[test]
 fn sort_by_cached_key() {
     let mut index_set: IndexSet<i32> = IndexSet::new();
     index_set.insert(3);

indexmap-rs/ringmap

(cherry picked from commit ab9e461b895184bd4926a8d4555f05d1a6c50666)

src/map.rs
@@ -1249,6 +1249,20 @@ impl<K, V, S> RingMap<K, V, S> {
         IntoIter::new(entries)
     }
 
+    /// Sort the map's key-value pairs in place using a sort-key extraction function.
+    ///
+    /// Computes in **O(n log n + c)** time and **O(n)** space where *n* is
+    /// the length of the map and *c* the capacity. The sort is stable.
+    pub fn sort_by_key<T, F>(&mut self, mut sort_key: F)
+    where
+        T: Ord,
+        F: FnMut(&K, &V) -> T,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.sort_by_key(move |a| sort_key(&a.key, &a.value));
+        });
+    }
+
     /// Sort the map's key-value pairs by the default ordering of the keys, but
     /// may not preserve the order of equal elements.
     ///
@@ -1293,6 +1307,20 @@ impl<K, V, S> RingMap<K, V, S> {
             .make_contiguous()
             .sort_unstable_by(move |a, b| cmp(&a.key, &a.value, &b.key, &b.value));
         IntoIter::new(entries)
+    }
+
+    /// Sort the map's key-value pairs in place using a sort-key extraction function.
+    ///
+    /// Computes in **O(n log n + c)** time where *n* is
+    /// the length of the map and *c* is the capacity. The sort is unstable.
+    pub fn sort_unstable_by_key<T, F>(&mut self, mut sort_key: F)
+    where
+        T: Ord,
+        F: FnMut(&K, &V) -> T,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.sort_unstable_by_key(move |a| sort_key(&a.key, &a.value));
+        });
     }
 
     /// Sort the map's key-value pairs in place using a sort-key extraction function.

src/rayon/map.rs
@@ -584,6 +584,18 @@ where
         IntoParIter { entries }
     }
 
+    /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction
+    /// function.
+    pub fn par_sort_by_key<T, F>(&mut self, sort_key: F)
+    where
+        T: Ord,
+        F: Fn(&K, &V) -> T + Sync,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.par_sort_by_key(move |a| sort_key(&a.key, &a.value));
+        });
+    }
+
     /// Sort the map's key-value pairs in parallel, by the default ordering of the keys.
     pub fn par_sort_unstable_keys(&mut self)
     where
@@ -619,6 +631,18 @@ where
             .make_contiguous()
             .par_sort_unstable_by(move |a, b| cmp(&a.key, &a.value, &b.key, &b.value));
         IntoParIter { entries }
+    }
+
+    /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction
+    /// function.
+    pub fn par_sort_unstable_by_key<T, F>(&mut self, sort_key: F)
+    where
+        T: Ord,
+        F: Fn(&K, &V) -> T + Sync,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.par_sort_unstable_by_key(move |a| sort_key(&a.key, &a.value));
+        });
     }
 
     /// Sort the map's key-value pairs in place and in parallel, using a sort-key extraction

src/rayon/set.rs
@@ -536,6 +536,17 @@ where
         IntoParIter { entries }
     }
 
+    /// Sort the set's values in place and in parallel, using a key extraction function.
+    pub fn par_sort_by_key<K, F>(&mut self, sort_key: F)
+    where
+        K: Ord,
+        F: Fn(&T) -> K + Sync,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.par_sort_by_key(move |a| sort_key(&a.key));
+        });
+    }
+
     /// Sort the set's values in parallel by their default ordering.
     pub fn par_sort_unstable(&mut self)
     where
@@ -567,6 +578,17 @@ where
             .make_contiguous()
             .par_sort_unstable_by(move |a, b| cmp(&a.key, &b.key));
         IntoParIter { entries }
+    }
+
+    /// Sort the set's values in place and in parallel, using a key extraction function.
+    pub fn par_sort_unstable_by_key<K, F>(&mut self, sort_key: F)
+    where
+        K: Ord,
+        F: Fn(&T) -> K + Sync,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.par_sort_unstable_by_key(move |a| sort_key(&a.key));
+        });
     }
 
     /// Sort the set's values in place and in parallel, using a key extraction function.

src/set.rs
@@ -1016,6 +1016,19 @@ impl<T, S> RingSet<T, S> {
         IntoIter::new(entries)
     }
 
+    /// Sort the set's values in place using a key extraction function.
+    ///
+    /// Computes in **O(n log n)** time and **O(n)** space. The sort is stable.
+    pub fn sort_by_key<K, F>(&mut self, mut sort_key: F)
+    where
+        K: Ord,
+        F: FnMut(&T) -> K,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.sort_by_key(move |a| sort_key(&a.key));
+        });
+    }
+
     /// Sort the set's values by their default ordering.
     ///
     /// See [`sort_unstable_by`](Self::sort_unstable_by) for details.
@@ -1047,6 +1060,19 @@ impl<T, S> RingSet<T, S> {
             .make_contiguous()
             .sort_unstable_by(move |a, b| cmp(&a.key, &b.key));
         IntoIter::new(entries)
+    }
+
+    /// Sort the set's values in place using a key extraction function.
+    ///
+    /// Computes in **O(n log n)** time. The sort is unstable.
+    pub fn sort_unstable_by_key<K, F>(&mut self, mut sort_key: F)
+    where
+        K: Ord,
+        F: FnMut(&T) -> K,
+    {
+        self.with_contiguous_entries(move |entries| {
+            entries.sort_unstable_by_key(move |a| sort_key(&a.key));
+        });
     }
 
     /// Sort the set's values in place using a key extraction function.

src/set/tests.rs
@@ -867,6 +867,28 @@ fn front() {
 }
 
 #[test]
+fn sort_by_key() {
+    let mut index_set: RingSet<i32> = RingSet::new();
+    index_set.insert(3);
+    index_set.insert(1);
+    index_set.insert(2);
+    index_set.insert(0);
+    index_set.sort_by_key(|&x| -x);
+    assert_eq!(index_set.make_contiguous(), &[3, 2, 1, 0]);
+}
+
+#[test]
+fn sort_unstable_by_key() {
+    let mut index_set: RingSet<i32> = RingSet::new();
+    index_set.insert(3);
+    index_set.insert(1);
+    index_set.insert(2);
+    index_set.insert(0);
+    index_set.sort_unstable_by_key(|&x| -x);
+    assert_eq!(index_set.make_contiguous(), &[3, 2, 1, 0]);
+}
+
+#[test]
 fn sort_by_cached_key() {
     let mut index_set: RingSet<i32> = RingSet::new();
     index_set.insert(3);