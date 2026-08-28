indexmap-rs/ordermap

fix clippy::useless_vec (and more) in tests

src/map/tests.rs
@@ -43,8 +43,8 @@ fn insert() {
 
 #[test]
 fn insert_full() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut map = IndexMap::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -327,11 +327,11 @@ fn partial_eq_and_eq() {
 #[test]
 fn extend() {
     let mut map = IndexMap::new();
-    map.extend(vec![(&1, &2), (&3, &4)]);
-    map.extend(vec![(5, 6)]);
+    map.extend([(&1, &2), (&3, &4)]);
+    map.extend([(5, 6)]);
     assert_eq!(
         map.into_iter().collect::<Vec<_>>(),
-        vec![(1, 2), (3, 4), (5, 6)]
+        [(1, 2), (3, 4), (5, 6)]
     );
 }
 
@@ -485,8 +485,7 @@ fn from_entries() {
 
 #[test]
 fn keys() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: IndexMap<_, _> = vec.into_iter().collect();
+    let map = IndexMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let keys: Vec<_> = map.keys().copied().collect();
     assert_eq!(keys.len(), 3);
     assert!(keys.contains(&1));
@@ -496,8 +495,7 @@ fn keys() {
 
 #[test]
 fn into_keys() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: IndexMap<_, _> = vec.into_iter().collect();
+    let map = IndexMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let keys: Vec<i32> = map.into_keys().collect();
     assert_eq!(keys.len(), 3);
     assert!(keys.contains(&1));
@@ -507,8 +505,7 @@ fn into_keys() {
 
 #[test]
 fn values() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: IndexMap<_, _> = vec.into_iter().collect();
+    let map = IndexMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let values: Vec<_> = map.values().copied().collect();
     assert_eq!(values.len(), 3);
     assert!(values.contains(&'a'));
@@ -518,8 +515,7 @@ fn values() {
 
 #[test]
 fn values_mut() {
-    let vec = vec![(1, 1), (2, 2), (3, 3)];
-    let mut map: IndexMap<_, _> = vec.into_iter().collect();
+    let mut map = IndexMap::<_, _>::from_iter([(1, 1), (2, 2), (3, 3)]);
     for value in map.values_mut() {
         *value *= 2
     }
@@ -532,8 +528,7 @@ fn values_mut() {
 
 #[test]
 fn into_values() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: IndexMap<_, _> = vec.into_iter().collect();
+    let map = IndexMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let values: Vec<char> = map.into_values().collect();
     assert_eq!(values.len(), 3);
     assert!(values.contains(&'a'));
@@ -698,8 +693,7 @@ fn shift_remove_full() {
 
 #[test]
 fn sorted_unstable_by() {
-    let mut map: IndexMap<i32, i32> = IndexMap::new();
-    map.extend(vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    let map = IndexMap::<i32, i32>::from_iter([(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
     let sorted = map.sorted_unstable_by(|_a, b, _c, d| d.cmp(&b));
 
     assert_eq!(
@@ -750,29 +744,24 @@ fn insert_before_oob() {
 
 #[test]
 fn clear() {
-    let mut map: IndexMap<i32, i32> = IndexMap::new();
-    map.extend(vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    let mut map = IndexMap::<i32, i32>::from_iter([(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    assert_ne!(map.len(), 0);
     map.clear();
     assert_eq!(map.len(), 0);
 }
 
 #[test]
 fn get_range() {
-    let mut index_map: IndexMap<i32, i32> = IndexMap::new();
-    index_map.insert(1, 10);
-    index_map.insert(2, 20);
-    index_map.insert(3, 30);
-    index_map.insert(4, 40);
-    index_map.insert(5, 50);
+    let map = IndexMap::<i32, i32>::from_iter([(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
 
-    let result = index_map.get_range(2..2);
+    let result = map.get_range(2..2);
     assert!(result.unwrap().is_empty());
 
     #[expect(clippy::reversed_empty_ranges)]
-    let result = index_map.get_range(4..2);
+    let result = map.get_range(4..2);
     assert!(result.is_none());
 
-    let result = index_map.get_range(2..4);
+    let result = map.get_range(2..4);
     let slice: &Slice<i32, i32> = result.unwrap();
     assert_eq!(slice.len(), 2);
     assert_eq!(slice, &[(3, 30), (4, 40)]);

src/set/tests.rs
@@ -42,8 +42,8 @@ fn insert() {
 
 #[test]
 fn insert_full() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = IndexSet::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -90,8 +90,7 @@ fn insert_2() {
 
 #[test]
 fn insert_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: IndexSet<u8> = elements.drain(..).collect();
+    let mut set = IndexSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -175,8 +174,8 @@ fn replace() {
 
 #[test]
 fn replace_full() {
-    let replace = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let replace = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = IndexSet::with_capacity(replace.len());
 
     for (i, &elt) in replace.iter().enumerate() {
@@ -223,8 +222,7 @@ fn replace_2() {
 
 #[test]
 fn replace_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: IndexSet<u8> = elements.drain(..).collect();
+    let mut set = IndexSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -426,9 +424,9 @@ fn partial_eq_and_eq() {
 #[test]
 fn extend() {
     let mut set = IndexSet::new();
-    set.extend(vec![&1, &2, &3, &4]);
-    set.extend(vec![5, 6]);
-    assert_eq!(set.into_iter().collect::<Vec<_>>(), vec![1, 2, 3, 4, 5, 6]);
+    set.extend([&1, &2, &3, &4]);
+    set.extend([5, 6]);
+    assert_eq!(set.into_iter().collect::<Vec<_>>(), [1, 2, 3, 4, 5, 6]);
 }
 
 #[test]

indexmap-rs/ringmap

(cherry picked from commit 21963654e41f86f396a2ba6d9582ccd8eb7df52c)

src/map/tests.rs
@@ -45,8 +45,8 @@ fn insert() {
 
 #[test]
 fn push_back() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut map = RingMap::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -69,8 +69,8 @@ fn push_back() {
 
 #[test]
 fn push_front() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut map = RingMap::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -447,11 +447,11 @@ fn partial_eq_and_eq() {
 #[test]
 fn extend() {
     let mut map = RingMap::new();
-    map.extend(vec![(&1, &2), (&3, &4)]);
-    map.extend(vec![(5, 6)]);
+    map.extend([(&1, &2), (&3, &4)]);
+    map.extend([(5, 6)]);
     assert_eq!(
         map.into_iter().collect::<Vec<_>>(),
-        vec![(1, 2), (3, 4), (5, 6)]
+        [(1, 2), (3, 4), (5, 6)]
     );
 }
 
@@ -628,8 +628,7 @@ fn from_entries() {
 
 #[test]
 fn keys() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: RingMap<_, _> = vec.into_iter().collect();
+    let map = RingMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let keys: Vec<_> = map.keys().copied().collect();
     assert_eq!(keys.len(), 3);
     assert!(keys.contains(&1));
@@ -639,8 +638,7 @@ fn keys() {
 
 #[test]
 fn into_keys() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: RingMap<_, _> = vec.into_iter().collect();
+    let map = RingMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let keys: Vec<i32> = map.into_keys().collect();
     assert_eq!(keys.len(), 3);
     assert!(keys.contains(&1));
@@ -650,8 +648,7 @@ fn into_keys() {
 
 #[test]
 fn values() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: RingMap<_, _> = vec.into_iter().collect();
+    let map = RingMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let values: Vec<_> = map.values().copied().collect();
     assert_eq!(values.len(), 3);
     assert!(values.contains(&'a'));
@@ -661,8 +658,7 @@ fn values() {
 
 #[test]
 fn values_mut() {
-    let vec = vec![(1, 1), (2, 2), (3, 3)];
-    let mut map: RingMap<_, _> = vec.into_iter().collect();
+    let mut map = RingMap::<_, _>::from_iter([(1, 1), (2, 2), (3, 3)]);
     for value in map.values_mut() {
         *value *= 2
     }
@@ -675,8 +671,7 @@ fn values_mut() {
 
 #[test]
 fn into_values() {
-    let vec = vec![(1, 'a'), (2, 'b'), (3, 'c')];
-    let map: RingMap<_, _> = vec.into_iter().collect();
+    let map = RingMap::<_, _>::from_iter([(1, 'a'), (2, 'b'), (3, 'c')]);
     let values: Vec<char> = map.into_values().collect();
     assert_eq!(values.len(), 3);
     assert!(values.contains(&'a'));
@@ -841,8 +836,7 @@ fn shift_remove_full() {
 
 #[test]
 fn sorted_unstable_by() {
-    let mut map: RingMap<i32, i32> = RingMap::new();
-    map.extend(vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    let map = RingMap::<i32, i32>::from_iter([(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
     let sorted: Vec<_> = map.sorted_unstable_by(|_a, b, _c, d| d.cmp(&b)).collect();
 
     assert_eq!(
@@ -893,8 +887,8 @@ fn insert_before_oob() {
 
 #[test]
 fn clear() {
-    let mut map: RingMap<i32, i32> = RingMap::new();
-    map.extend(vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    let mut map = RingMap::<i32, i32>::from_iter([(1, 10), (2, 20), (3, 30), (4, 40), (5, 50)]);
+    assert_ne!(map.len(), 0);
     map.clear();
     assert_eq!(map.len(), 0);
 }

src/set/tests.rs
@@ -44,8 +44,8 @@ fn insert() {
 
 #[test]
 fn push_back() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = RingSet::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -68,8 +68,8 @@ fn push_back() {
 
 #[test]
 fn push_front() {
-    let insert = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let insert = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = RingSet::with_capacity(insert.len());
 
     for (i, &elt) in insert.iter().enumerate() {
@@ -117,8 +117,7 @@ fn insert_2() {
 
 #[test]
 fn insert_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: RingSet<u8> = elements.drain(..).collect();
+    let mut set = RingSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -202,8 +201,8 @@ fn replace() {
 
 #[test]
 fn replace_full() {
-    let replace = vec![9, 2, 7, 1, 4, 6, 13];
-    let present = vec![1, 6, 2];
+    let replace = [9, 2, 7, 1, 4, 6, 13];
+    let present = [1, 6, 2];
     let mut set = RingSet::with_capacity(replace.len());
 
     for (i, &elt) in replace.iter().enumerate() {
@@ -250,8 +249,7 @@ fn replace_2() {
 
 #[test]
 fn replace_dup() {
-    let mut elements = vec![0, 2, 4, 6, 8];
-    let mut set: RingSet<u8> = elements.drain(..).collect();
+    let mut set = RingSet::<u8>::from_iter([0, 2, 4, 6, 8]);
     {
         let (i, v) = set.get_full(&0).unwrap();
         assert_eq!(set.len(), 5);
@@ -550,9 +548,9 @@ fn partial_eq_and_eq() {
 #[test]
 fn extend() {
     let mut set = RingSet::new();
-    set.extend(vec![&1, &2, &3, &4]);
-    set.extend(vec![5, 6]);
-    assert_eq!(set.into_iter().collect::<Vec<_>>(), vec![1, 2, 3, 4, 5, 6]);
+    set.extend([&1, &2, &3, &4]);
+    set.extend([5, 6]);
+    assert_eq!(set.into_iter().collect::<Vec<_>>(), [1, 2, 3, 4, 5, 6]);
 }
 
 #[test]