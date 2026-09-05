indexmap-rs/ordermap

Fix item hygiene in user macros

src/macros.rs
@@ -23,11 +23,14 @@
 macro_rules! indexmap_with_default {
     ($H:ty; $($key:expr => $value:expr,)+) => { $crate::indexmap_with_default!($H; $($key => $value),+) };
     ($H:ty; $($key:expr => $value:expr),*) => {{
-        let builder = ::core::hash::BuildHasherDefault::<$H>::new();
-        const CAP: usize = <[()]>::len(&[$({ stringify!($key); }),*]);
         #[allow(unused_mut)]
-        // Specify your custom `H` (must implement Default + Hasher) as the hasher:
-        let mut map = $crate::IndexMap::with_capacity_and_hasher(CAP, builder);
+        let mut map = $crate::IndexMap::with_capacity_and_hasher(
+            // Note: `stringify!($key)` is just here to consume the repetition,
+            // but we throw away that string literal during constant evaluation.
+            const { <[()]>::len(&[$({ stringify!($key); }),*]) },
+            // Specify your custom `H` (must implement Default + Hasher) as the hasher:
+            ::core::hash::BuildHasherDefault::<$H>::new(),
+        );
         $(
             map.insert($key, $value);
         )*
@@ -58,18 +61,17 @@ macro_rules! indexmap_with_default {
 /// ```
 macro_rules! indexmap {
     ($($key:expr => $value:expr,)+) => { $crate::indexmap!($($key => $value),+) };
-    ($($key:expr => $value:expr),*) => {
-        {
+    ($($key:expr => $value:expr),*) => {{
+        let mut map = $crate::IndexMap::with_capacity(
             // Note: `stringify!($key)` is just here to consume the repetition,
             // but we throw away that string literal during constant evaluation.
-            const CAP: usize = <[()]>::len(&[$({ stringify!($key); }),*]);
-            let mut map = $crate::IndexMap::with_capacity(CAP);
-            $(
-                map.insert($key, $value);
-            )*
-            map
-        }
-    };
+            const { <[()]>::len(&[$({ stringify!($key); }),*]) },
+        );
+        $(
+            map.insert($key, $value);
+        )*
+        map
+    }};
 }
 
 /// Create an [`IndexSet`][crate::IndexSet] from a list of values
@@ -97,11 +99,14 @@ macro_rules! indexmap {
 macro_rules! indexset_with_default {
     ($H:ty; $($value:expr,)+) => { $crate::indexset_with_default!($H; $($value),+) };
     ($H:ty; $($value:expr),*) => {{
-        let builder = ::core::hash::BuildHasherDefault::<$H>::new();
-        const CAP: usize = <[()]>::len(&[$({ stringify!($value); }),*]);
         #[allow(unused_mut)]
-        // Specify your custom `H` (must implement Default + Hash) as the hasher:
-        let mut set = $crate::IndexSet::with_capacity_and_hasher(CAP, builder);
+        let mut set = $crate::IndexSet::with_capacity_and_hasher(
+            // Note: `stringify!($value)` is just here to consume the repetition,
+            // but we throw away that string literal during constant evaluation.
+            const { <[()]>::len(&[$({ stringify!($value); }),*]) },
+            // Specify your custom `H` (must implement Default + Hash) as the hasher:
+            ::core::hash::BuildHasherDefault::<$H>::new(),
+        );
         $(
             set.insert($value);
         )*
@@ -132,18 +137,17 @@ macro_rules! indexset_with_default {
 /// ```
 macro_rules! indexset {
     ($($value:expr,)+) => { $crate::indexset!($($value),+) };
-    ($($value:expr),*) => {
-        {
+    ($($value:expr),*) => {{
+        let mut set = $crate::IndexSet::with_capacity(
             // Note: `stringify!($value)` is just here to consume the repetition,
             // but we throw away that string literal during constant evaluation.
-            const CAP: usize = <[()]>::len(&[$({ stringify!($value); }),*]);
-            let mut set = $crate::IndexSet::with_capacity(CAP);
-            $(
-                set.insert($value);
-            )*
-            set
-        }
-    };
+            const { <[()]>::len(&[$({ stringify!($value); }),*]) },
+        );
+        $(
+            set.insert($value);
+        )*
+        set
+    }};
 }
 
 // generate all the Iterator methods by just forwarding to the underlying

tests/macros_full_path.rs
@@ -17,3 +17,43 @@ fn test_create_set() {
         3,
     };
 }
+
+#[test]
+fn test_map_shadow() {
+    // The macro used to have its own `const CAP` which would shadow this, because items are not
+    // protected by macro hygiene. Now we avoid any items in the macro, and its local `map` *is*
+    // hygienic vs. the local `map` here.
+    const CAP: usize = 42;
+    let map = -1;
+    let m = indexmap::indexmap! {
+        map => CAP,
+    };
+    assert_eq!(m[&map], CAP);
+    assert_eq!(m[0], CAP);
+}
+
+#[test]
+fn test_map_shadow_default() {
+    const CAP: usize = 42;
+    let map = -1;
+    let m = indexmap::indexmap_with_default! {
+        fnv::FnvHasher;
+        map => CAP,
+    };
+    assert_eq!(m[&map], CAP);
+    assert_eq!(m[0], CAP);
+}
+
+#[test]
+fn test_set_shadow() {
+    const CAP: usize = 42;
+    let s = indexmap::indexset!(CAP);
+    assert_eq!(s[0], CAP);
+}
+
+#[test]
+fn test_set_shadow_default() {
+    const CAP: usize = 42;
+    let s = indexmap::indexset_with_default!(fnv::FnvHasher; CAP);
+    assert_eq!(s[0], CAP);
+}

indexmap-rs/ringmap

(cherry picked from commit c067355e1ca5ec39348f42dcee582d4f222c3295)

src/macros.rs
@@ -23,11 +23,14 @@
 macro_rules! ringmap_with_default {
     ($H:ty; $($key:expr => $value:expr,)+) => { $crate::ringmap_with_default!($H; $($key => $value),+) };
     ($H:ty; $($key:expr => $value:expr),*) => {{
-        let builder = ::core::hash::BuildHasherDefault::<$H>::new();
-        const CAP: usize = <[()]>::len(&[$({ stringify!($key); }),*]);
         #[allow(unused_mut)]
-        // Specify your custom `H` (must implement Default + Hasher) as the hasher:
-        let mut map = $crate::RingMap::with_capacity_and_hasher(CAP, builder);
+        let mut map = $crate::RingMap::with_capacity_and_hasher(
+            // Note: `stringify!($key)` is just here to consume the repetition,
+            // but we throw away that string literal during constant evaluation.
+            const { <[()]>::len(&[$({ stringify!($key); }),*]) },
+            // Specify your custom `H` (must implement Default + Hasher) as the hasher:
+            ::core::hash::BuildHasherDefault::<$H>::new(),
+        );
         $(
             map.insert($key, $value);
         )*
@@ -58,18 +61,17 @@ macro_rules! ringmap_with_default {
 /// ```
 macro_rules! ringmap {
     ($($key:expr => $value:expr,)+) => { $crate::ringmap!($($key => $value),+) };
-    ($($key:expr => $value:expr),*) => {
-        {
+    ($($key:expr => $value:expr),*) => {{
+        let mut map = $crate::RingMap::with_capacity(
             // Note: `stringify!($key)` is just here to consume the repetition,
             // but we throw away that string literal during constant evaluation.
-            const CAP: usize = <[()]>::len(&[$({ stringify!($key); }),*]);
-            let mut map = $crate::RingMap::with_capacity(CAP);
-            $(
-                map.insert($key, $value);
-            )*
-            map
-        }
-    };
+            const { <[()]>::len(&[$({ stringify!($key); }),*]) },
+        );
+        $(
+            map.insert($key, $value);
+        )*
+        map
+    }};
 }
 
 /// Create a [`RingSet`][crate::RingSet] from a list of values
@@ -97,11 +99,14 @@ macro_rules! ringmap {
 macro_rules! ringset_with_default {
     ($H:ty; $($value:expr,)+) => { $crate::ringset_with_default!($H; $($value),+) };
     ($H:ty; $($value:expr),*) => {{
-        let builder = ::core::hash::BuildHasherDefault::<$H>::new();
-        const CAP: usize = <[()]>::len(&[$({ stringify!($value); }),*]);
         #[allow(unused_mut)]
-        // Specify your custom `H` (must implement Default + Hash) as the hasher:
-        let mut set = $crate::RingSet::with_capacity_and_hasher(CAP, builder);
+        let mut set = $crate::RingSet::with_capacity_and_hasher(
+            // Note: `stringify!($value)` is just here to consume the repetition,
+            // but we throw away that string literal during constant evaluation.
+            const { <[()]>::len(&[$({ stringify!($value); }),*]) },
+            // Specify your custom `H` (must implement Default + Hash) as the hasher:
+            ::core::hash::BuildHasherDefault::<$H>::new(),
+        );
         $(
             set.insert($value);
         )*
@@ -132,18 +137,17 @@ macro_rules! ringset_with_default {
 /// ```
 macro_rules! ringset {
     ($($value:expr,)+) => { $crate::ringset!($($value),+) };
-    ($($value:expr),*) => {
-        {
+    ($($value:expr),*) => {{
+        let mut set = $crate::RingSet::with_capacity(
             // Note: `stringify!($value)` is just here to consume the repetition,
             // but we throw away that string literal during constant evaluation.
-            const CAP: usize = <[()]>::len(&[$({ stringify!($value); }),*]);
-            let mut set = $crate::RingSet::with_capacity(CAP);
-            $(
-                set.insert($value);
-            )*
-            set
-        }
-    };
+            const { <[()]>::len(&[$({ stringify!($value); }),*]) },
+        );
+        $(
+            set.insert($value);
+        )*
+        set
+    }};
 }
 
 // generate all the Iterator methods by just forwarding to the underlying

tests/macros_full_path.rs
@@ -17,3 +17,43 @@ fn test_create_set() {
         3,
     };
 }
+
+#[test]
+fn test_map_shadow() {
+    // The macro used to have its own `const CAP` which would shadow this, because items are not
+    // protected by macro hygiene. Now we avoid any items in the macro, and its local `map` *is*
+    // hygienic vs. the local `map` here.
+    const CAP: usize = 42;
+    let map = -1;
+    let m = ringmap::ringmap! {
+        map => CAP,
+    };
+    assert_eq!(m[&map], CAP);
+    assert_eq!(m[0], CAP);
+}
+
+#[test]
+fn test_map_shadow_default() {
+    const CAP: usize = 42;
+    let map = -1;
+    let m = ringmap::ringmap_with_default! {
+        fnv::FnvHasher;
+        map => CAP,
+    };
+    assert_eq!(m[&map], CAP);
+    assert_eq!(m[0], CAP);
+}
+
+#[test]
+fn test_set_shadow() {
+    const CAP: usize = 42;
+    let s = ringmap::ringset!(CAP);
+    assert_eq!(s[0], CAP);
+}
+
+#[test]
+fn test_set_shadow_default() {
+    const CAP: usize = 42;
+    let s = ringmap::ringset_with_default!(fnv::FnvHasher; CAP);
+    assert_eq!(s[0], CAP);
+}